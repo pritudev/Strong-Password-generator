bcd3fe89c8d9aca51abd4773ecd2fcbecff8599e
pritudev <contact.pritulx@gmail.com>
2021-09-06 20:27:31 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password");
 function genPassword() {
   var chars =
     "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%^&*(){}[]<>/:;',.";
-  var passwordLength = 12;
+  var passwordLength = 16;
   var password = "";
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
